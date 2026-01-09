2f9fcbe412ac3ac6e20ccc2b8c05d89bc07a31a8
Koul <dongik0122@gmail.com>
2026-01-09 09:44:31 +0900

    feat:BaseTimeEntity 도입
    - PersonalAppApplication에 @EnableJpaAuditing 추가
    - global/entity/BaseTimeEntity 생성 (@MappedSuperclass)

--- src/main/java/koul/PersonalApp/PersonalAppApplication.java
+++ src/main/java/koul/PersonalApp/PersonalAppApplication.java
@@ -2,8 +2,10 @@ package koul.PersonalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PersonalAppApplication {
 
 	public static void main(String[] args) {
